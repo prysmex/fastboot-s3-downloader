b3cdb4d89837a700d367477ccf21b15ee77305e9
betocantu93 <betocantu93@gmail.com>
2022-07-15 13:01:23 -0500

    optionalli install deps and fix outputPathFor

--- index.js
+++ index.js
@@ -1,138 +1,145 @@
-"use strict";
+'use strict';
 
-const AWS  = require('aws-sdk');
+const AWS = require('aws-sdk');
 const fs = require('fs');
 const path = require('path');
-const fsp  = require('fs-promise');
+const fsp = require('fs-promise');
 const exec = require('child_process').exec;
 
 function AppNotFoundError(message) {
-  let error = new Error(message);
-  error.name = 'AppNotFoundError';
+	let error = new Error(message);
+	error.name = 'AppNotFoundError';
 
-  return error;
+	return error;
 }
 /*
  * Downloader class that downloads the latest version of the deployed
  * app from S3 and unzips it.
  */
 class S3Downloader {
-  constructor(options) {
-    this.ui = options.ui;
-    this.configBucket = options.bucket;
-    this.configKey = options.key;
-    this.s3 = new AWS.S3({
-      apiVersion: '2006-03-01',
-      signatureVersion: 'v4',
-      accessKeyId: options.accessKeyId,
-      secretAccessKey: options.secretAccessKey,
-      region: options.region
-    });
-  }
+	constructor(options) {
+		this.ui = options.ui;
+		this.configBucket = options.bucket;
+		this.configKey = options.key;
+		this.installDependencies = options.installDependencies === false ? false : true;
+		this.s3 = new AWS.S3({
+			apiVersion: '2006-03-01',
+			signatureVersion: 'v4',
+			accessKeyId: options.accessKeyId,
+			secretAccessKey: options.secretAccessKey,
+			region: options.region,
+		});
+	}
 
-  download() {
-    if (!this.configBucket || !this.configKey) {
-      this.ui.writeError('no S3 bucket or key provided; not downloading app');
-      return Promise.reject(new AppNotFoundError());
-    }
+	download() {
+		if (!this.configBucket || !this.configKey) {
+			this.ui.writeError('no S3 bucket or key provided; not downloading app');
+			return Promise.reject(new AppNotFoundError());
+		}
 
-    return this.fetchCurrentVersion()
-      .then(() => this.removeOldApp())
-      .then(() => this.downloadAppZip())
-      .then(() => this.unzipApp())
-      .then(() => this.installNPMDependencies())
-      .then(() => this.outputPath);
-  }
+		return this.fetchCurrentVersion()
+			.then(() => this.removeOldApp())
+			.then(() => this.downloadAppZip())
+			.then(() => this.unzipApp())
+			.then(() => this.installNPMDependencies())
+			.then(() => this.outputPath);
+	}
 
-  removeOldApp() {
-    if (!this.outputPath) {
-      return Promise.resolve();
-    }
+	removeOldApp() {
+		if (!this.outputPath) {
+			return Promise.resolve();
+		}
 
-    this.ui.writeLine('removing ' + this.outputPath);
-    return fsp.remove(this.outputPath);
-  }
+		this.ui.writeLine('removing ' + this.outputPath);
+		return fsp.remove(this.outputPath);
+	}
 
-  fetchCurrentVersion() {
-    let bucket = this.configBucket;
-    let key = this.configKey;
+	fetchCurrentVersion() {
+		let bucket = this.configBucket;
+		let key = this.configKey;
 
-    this.ui.writeLine('fetching current app version from ' + bucket + '/' + key);
+		this.ui.writeLine('fetching current app version from ' + bucket + '/' + key);
 
-    let params = {
-      Bucket: bucket,
-      Key: key
-    };
+		let params = {
+			Bucket: bucket,
+			Key: key,
+		};
 
-    return this.s3.getObject(params).promise()
-      .then(data => {
-        let config = JSON.parse(data.Body);
-        this.ui.writeLine('got config', config);
+		return this.s3
+			.getObject(params)
+			.promise()
+			.then((data) => {
+				let config = JSON.parse(data.Body);
+				this.ui.writeLine('got config', config);
 
-        this.appBucket = config.bucket;
-        this.appKey = config.key;
-        this.zipPath = path.basename(config.key);
-        this.outputPath = outputPathFor(this.zipPath);
-      });
-  }
+				this.appBucket = config.bucket;
+				this.appKey = config.key;
+				this.zipPath = path.basename(config.key);
+				this.outputPath = outputPathFor(this.zipPath);
+			});
+	}
 
-  downloadAppZip() {
-    return new Promise((res, rej) => {
-      let bucket = this.appBucket;
-      let key = this.appKey;
+	downloadAppZip() {
+		return new Promise((res, rej) => {
+			let bucket = this.appBucket;
+			let key = this.appKey;
 
-      let params = {
-        Bucket: bucket,
-        Key: key
-      };
+			let params = {
+				Bucket: bucket,
+				Key: key,
+			};
 
-      let zipPath = this.zipPath;
-      let file = fs.createWriteStream(zipPath);
-      let request = this.s3.getObject(params);
+			let zipPath = this.zipPath;
+			let file = fs.createWriteStream(zipPath);
+			let request = this.s3.getObject(params);
 
-      this.ui.writeLine("saving S3 object " + bucket + "/" + key + " to " + zipPath);
+			this.ui.writeLine('saving S3 object ' + bucket + '/' + key + ' to ' + zipPath);
 
-      request.createReadStream().pipe(file)
-        .on('close', res)
-        .on('error', rej);
-    });
-  }
+			request.createReadStream().pipe(file).on('close', res).on('error', rej);
+		});
+	}
 
-  unzipApp() {
-    let zipPath = this.zipPath;
+	unzipApp() {
+		let zipPath = this.zipPath;
 
-    return this.exec('unzip -q ' + zipPath)
-      .then(() => {
-        this.ui.writeLine("unzipped " + zipPath);
-      });
-  }
+		return this.exec('unzip -q ' + zipPath).then(() => {
+			this.ui.writeLine('unzipped ' + zipPath);
+		});
+	}
 
-  installNPMDependencies() {
-    return this.exec(`cd ${this.outputPath} && npm install -s`)
-      .then(() => this.ui.writeLine('installed npm dependencies'))
-      .catch(() => this.ui.writeError('unable to install npm dependencies'));
-  }
+	installNPMDependencies() {
+		if (this.installDependencies) {
+			return this.exec(`cd ${this.outputPath} && npm install -s`)
+				.then(() => this.ui.writeLine('installed npm dependencies'))
+				.catch(() => this.ui.writeError('unable to install npm dependencies'));
+		}
+	}
 
-  exec(command) {
-    return new Promise((resolve, reject) => {
-      exec(command, (error, stdout, stderr) => {
-        if (error) {
-          this.ui.writeError(`error running command ${command}`);
-          this.ui.writeError(stderr);
-          reject(error);
-        } else {
-          resolve();
-        }
-      });
-    });
-  }
+	exec(command) {
+		return new Promise((resolve, reject) => {
+			exec(command, (error, stdout, stderr) => {
+				if (error) {
+					this.ui.writeError(`error running command ${command}`);
+					this.ui.writeError(stderr);
+					reject(error);
+				} else {
+					resolve();
+				}
+			});
+		});
+	}
 }
 
 function outputPathFor(zipPath) {
-  let name = path.basename(zipPath, '.zip');
+	let name = path.basename(zipPath, '.zip');
 
-  // Remove MD5 hash
-  return name.split('-').slice(0, -1).join('-');
+	// Remove MD5 hash
+	let removed = name.split('-');
+	if (removed.length > 1) {
+		removed = removed.slice(0, -1);
+	}
+	removed = removed.join('-');
+	return removed;
 }
 
 module.exports = S3Downloader;
